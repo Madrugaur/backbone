fdd2a0a1522080d52dc19008e214c06590d4c4fe
Braden Little <37552259+Madrugaur@users.noreply.github.com>
2021-03-25 17:55:10 -0400

    started working on script to find common wiki tag for women

---
+++ code/python/scrape/findCommonAttributeForWomen.py
@@ -0,0 +1,27 @@
+"""
+brief: the purpose of this script is to assert there is a/are common attribute(s) attached to all of the linked
+        articles under
+        <a href="https://en.wikipedia.org/wiki/Index_of_women_scientists_articles">Index of Women Scientists Articles</a>
+        If such an attribute or attributes exists, I can reasonably assume that it or they will apply to all of the
+        articles about women in other Indices or Lists.
+author: Braden Little
+"""
+
+import bs4
+import requests
+
+base_url = "https://en.wikipedia.org"
+
+response = requests.get("https://en.wikipedia.org/wiki/Index_of_women_scientists_articles")
+
+if response is not None:
+    page = bs4.BeautifulSoup(response.text, "html.parser")
+    cols = page.find_all("div", class_="div-col")
+
+    for col in cols:
+        list_elements = col.find_all("li")
+        for element in list_elements:
+            link = element.select("a")[0]["href"]
+            subpage = bs4.BeautifulSoup(requests.get(base_url + link))
+
+
