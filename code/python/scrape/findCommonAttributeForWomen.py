537bd2aad5a0cf81d0abd5c638c5d23975994c31
Braden Little <blittle485@gmail.com>
2021-03-26 10:53:29 -0400

    included external research

--- code/python/scrape/findCommonAttributeForWomen.py
+++ code/python/scrape/findCommonAttributeForWomen.py
@@ -1,9 +1,15 @@
 """
-brief: the purpose of this script is to assert there is a/are common attribute(s) attached to all of the linked
+brief: the purpose of this script is to assert there are any Categories attached to all of the linked
         articles under
         <a href="https://en.wikipedia.org/wiki/Index_of_women_scientists_articles">Index of Women Scientists Articles</a>
-        If such an attribute or attributes exists, I can reasonably assume that it or they will apply to all of the
+        If such Categories exists, I can reasonably assume that they will apply to all of the
         articles about women in other Indices or Lists.
+
+conclusion: Fun fact, there are no common Categories for women scientist on Wikipedia, or women in general it seems. In
+        running this experiment I have discovered that there is little consistent category usage which makes my job
+        much harder. So bios about women don't have any categories relating to women attached. So what I'm going to do
+        is crawl through all the bios relating to people in STEM and then try to discover a reliable way of dividing
+        those by gender
 author: Braden Little
 """
 
@@ -16,14 +22,22 @@ response = requests.get("https://en.wikipedia.org/wiki/Index_of_women_scientists
 
 if response is not None:
     page = bs4.BeautifulSoup(response.text, "html.parser")
-    cols = page.find_all("div", class_="div-col")
-
-    for col in cols:
-        list_elements = col.find_all("li")
-        for element in list_elements:
-            link = element.select("a")[0]["href"]
-            subpage = bs4.BeautifulSoup(requests.get(base_url + link))
-            cats = subpage.select("div", class_="catlinks")[0]
-            print(cats)
-
-
+    content_boxes = page.find_all("div", class_="div-col")
+    attribute_count_map = dict()
+    subpages = 0
+    for content_box in content_boxes:
+        people_items = content_box.find_all("li")
+        for person in people_items:
+            bio_link = person.select("a")[0]["href"]
+            bio_page = bs4.BeautifulSoup(requests.get(base_url + bio_link).text, "html.parser")
+            attribute_box = bio_page.find(id="mw-normal-catlinks")
+            if attribute_box is not None:
+                attributes = attribute_box.find_all("li")
+                subpages += 1
+                has_women_attribute = False
+                for attribute in attributes:
+                    attribute_name = attribute.text
+                    if "women" in attribute_name.lower():
+                        has_women_attribute = True
+                if not has_women_attribute:
+                    print(bio_link)
