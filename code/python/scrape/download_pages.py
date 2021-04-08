3e118e3c938e8cb4dbd9433932b77a211dc11b37
Braden Little <blittle485@gmail.com>
2021-04-08 09:11:35 -0400

    data collection

--- code/python/scrape/download_pages.py
+++ code/python/scrape/download_pages.py
@@ -1,13 +1,45 @@
 import bs4
 import os
+import json
+
+json_template = """
+{
+"name": "{name},
+"content": "{content}",
+"tags": "{tags}",
+"raw": "{raw}"
+}
+"""
 
 
 def start_save_process(page):
     page_name = page.find(id="firstHeading").text
+    json_obj = {
+        "name": page_name,
+        "content": get_content(page),
+        "tags": get_tags(page),
+        "raw": page.find(class_="mw-parser-output").text
+    }
+
     f_name = os.path.join(os.getcwd(), "data/people/people_" + page_name + ".txt")
     f = open(f_name, "w", encoding="utf8")
-    f.write(page.find(class_="mw-parser-output").text)
-    print(f_name)
+    f.write(json.dumps(json_obj))
 
 
+def get_tags(page):
+    category_box = page.find(id="catlinks")
+    list_of_category_items = category_box.find_all("li")
+    str_tag_list = list()
+    for item in list_of_category_items:
+        link = item.find("a")
+        str_tag_list.append(link.text)
+    return str_tag_list
 
+
+def get_content(page):
+    raw_content = page.find(class_="mw-parser-output")
+    paragraphs = raw_content.find_all("p")
+    content = ""
+    for p in paragraphs:
+        content += p.text + " "
+    return content
--- code/python/scrape/wiki_crawler.py
+++ code/python/scrape/wiki_crawler.py
@@ -1,19 +1,23 @@
 import bs4
 import requests
-from download_pages import start_save_process
+from download_pages import start_save_process, get_tags
+import traceback
 
-
-root_url = "https://en.wikipedia.org/wiki/List_of_people_considered_father_or_mother_of_a_scientific_field"
+root_url = "https://en.wikipedia.org/wiki/Category:Lists_of_people_in_STEM_fields"
 base_url = "https://en.wikipedia.org"
 
 # Might remove in favor having this redundancy filtering being done at download
 have_visited = dict()
 
-pages_to_ignore = ["List of people considered father or mother of a field"]
+pages_to_ignore = []
+
+headings_to_ignore = ["Editing:", "Portal:", "Wikipedia:", "Talk:", "File:", "Template:"]
 
 
-def descend(page):
+def descend(page, level: int, is_redirect=False):
     try:
+        if level >= 4:
+            return
         if page is not None:
             heading = page.find(id="firstHeading")
             heading_text = heading.text
@@ -21,65 +25,86 @@ def descend(page):
                 return
             elif page in have_visited.keys():
                 return
-            elif "Editing" in heading_text:
+            elif [element for element in headings_to_ignore if (element in heading_text)]:
                 return
-            elif "Portal" in heading_text:
-                return
-            elif "Category" in heading_text:
-                handle_category(page)
-            elif "Lists of" in heading_text:
-                handle_lists(page)
-            elif "List of" in heading_text:
-                handle_list(page)
             else:
-                handle_ambiguous_page(page)
+                print("\n|", 2 * level * "—", heading_text, sep="", end="")
+                if "Category" in heading_text:
+                    handle_category(page, level + 1)
+                elif "Lists of" in heading_text and not is_redirect:
+                    handle_lists(page, level + 1)
+                elif "List of" in heading_text and not is_redirect:
+                    handle_list(page, level + 1)
+                else:
+                    handle_ambiguous_page(page, level + 1)
+
     except Exception as e:
+        print("")
         print(e)
 
 
-def handle_ambiguous_page(page):
+def handle_ambiguous_page(page, level):
     biography_infobox = page.find(class_="infobox biography vcard")
-    if biography_infobox is not None:
+
+    if biography_infobox is not None or prob_is_a_person(page):
         have_visited[page] = True
         start_save_process(page)
+        print(": Saved", end="")
         return
 
 
-def handle_category(page):
+def prob_is_a_person(page):
+    tags = get_tags(page)
+    str_tags = " ".join(tags)
+    personhood_markers = ["living ", " births", " deaths"]
+    for marker in personhood_markers:
+        if marker in str_tags.lower():
+            return True
+    return False
+
+
+def handle_category(page, level: int):
     sub_pages = page.find(id="mw-pages")
-    links_to_subpages = sub_pages.find_all("li")
-    for link in links_to_subpages:
+    list_items = sub_pages.find_all("li")
+    for item in list_items:
         have_visited[page] = True
+        link = item.find("a")
         url = base_url + link["href"]
-        descend(get_bs4_page(url))
+        descend(get_bs4_page(url), level)
 
 
-def handle_lists(page):
+def handle_lists(page, level: int):
     content = page.find(class_="mw-parser-output")
     list_items = content.find_all("li")
     for list_item in list_items:
         have_visited[page] = True
         link = list_item.find("a")
         url = base_url + link["href"]
-        descend(get_bs4_page(url))
+        descend(get_bs4_page(url), level)
 
 
-def handle_list(page):
+def handle_list(page, level: int):
     content = page.find(class_="mw-parser-output")
     if content is None:
         return
     link_to_pages = content.find_all("a")
     for link in link_to_pages:
         href = link["href"]
-        if "#" not in href:
+        is_redirect = False
+        if link.has_attr("class"):
+            is_redirect = ("mw-redirect" in link["class"])
+        if ("#" not in href) and (link.text != "link") and ("List of" not in link.text) \
+                and ("Lists of" not in link.text) and ("Category" not in link.text):
             have_visited[page] = True
-            descend(get_bs4_page(base_url + href))
+            descend(get_bs4_page(base_url + href), level, is_redirect)
 
 
 def get_bs4_page(url: str):
+    if "/wiki/" not in url:
+        return None
     response = requests.get(url)
     return bs4.BeautifulSoup(response.text, "html.parser")
 
 
 if __name__ == '__main__':
-    descend(get_bs4_page(root_url))
+    descend(get_bs4_page(root_url), 0)
